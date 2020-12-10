766291a6e33ba3d0bfd7d8e7e98f0b9246608e3e
Alisherbek Rakhimov <alisherrakhimov1997@gmail.com>
2020-12-10 18:23:24 +0500

    sql dump added

---
+++ news_category.sql
@@ -0,0 +1,5 @@
+INSERT INTO news_category (id, title) VALUES (1, 'kultura');
+INSERT INTO news_category (id, title) VALUES (2, 'Sport');
+INSERT INTO news_category (id, title) VALUES (3, 'Politika');
+INSERT INTO news_category (id, title) VALUES (4, 'Nauka');
+INSERT INTO news_category (id, title) VALUES (5, 'Test');
